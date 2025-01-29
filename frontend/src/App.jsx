1f8117f0ac78ed25ec4cf369a92ea0d6d86c986e
Kunal Singh <kunalindia59@gmail.com>
2025-01-29 22:12:48 +0530

    added landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import {Route, Routes} from "react-router-dom"
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import LandingPage from './pages/LandingPage'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isOnline , setIsOnline] = useState(false);
+  useEffect(()=>{
+    const token = localStorage.getItem('token')
+    if(!token){
+      setIsOnline(false);
+    }
+    else {
+      setIsOnline(true);
+    }
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+
+      <Route path='/' element={<LandingPage/>} />
+
+      <Route path='/signup' element={ isOnline ?<div>Hello</div> : <Signup setIsOnline={setIsOnline} /> } />
+      <Route path='/signin' element={ isOnline ?<div>Hello</div> : <Signin setIsOnline={setIsOnline} /> } />
+
+    </Routes>
   )
 }
 
